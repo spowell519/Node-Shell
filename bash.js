3f346dec53f65ccfd7213a97d80e4652283b22ad
Jocelyn Icaza <jocelyn.icaza@gmail.com>
2022-03-04 12:14:01 -0800

    pwd command

--- bash.js
+++ bash.js
@@ -3,6 +3,11 @@ process.stdout.write('prompt > ');
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
 
-    process.stdout.write('You typed: ' + cmd);
+    if(cmd === 'pwd'){
+      console.log(process.cwd());
+    }
+
+    //process.stdout.write('You typed: ' + cmd);
     process.stdout.write('\nprompt > ');
 })
+
